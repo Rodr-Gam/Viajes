d1416a957bbba982167a4e94237f921bb4e9cb26
MaryGomez19 <2123200430@soy.utj.edu.mx>
2026-05-20 11:34:52 -0600

    Modulo de reservaciones

---
+++ app/Models/Reservation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    protected $fillable = [
+        'reference_person',
+        'reservation_date',
+        'departure_date',
+        'return_date',
+        'state',
+        'observations',
+    ];
+
+    /*public function package()
+    {
+        return $this->belongsTo(Package::class);
+    }*/
+}
--- routes/api.php
+++ routes/api.php
@@ -6,13 +6,15 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\CityController;
-
+use App\Http\Controllers\ReservationController;
 
 Route::post('/cities', [CityController::class, 'store']);
 Route::post('/hotels', [HotelController::class, 'store']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/roles', [RoleController::class, 'store']);
+Route::post('/reservations', [ReservationController::class, 'store'])->middleware('auth:sanctum');
+Route::get('/reservations', [ReservationController::class, 'index'])->middleware('auth:sanctum');
 
 Route::middleware('auth:sanctum')->get('/perfil', function (Request $request) {
     return $request->user();
